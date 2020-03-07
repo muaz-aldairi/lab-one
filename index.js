2536948f2edd99ed3da113de41502c4d2aad5596
Muaz Aldairi <muaz-aldairi@live.nmit.ac.nz>
2020-03-07 20:19:44 +1300

    Basic Mathematical Operators

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var number_one= 10;
+var number_two= 6;
+
+console.log("multi =" + number_one * number_two );
+console.log("div =" + number_one / number_two );
+console.log("sum =" + (number_one + number_two) );
+console.log("sub =" + (number_one - number_two) );
+
+
+
+
